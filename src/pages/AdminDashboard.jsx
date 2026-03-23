d6b6a8794e3e474da06f9d3eb209a3e110b4c498
kash2021-hobby <kashyapnandan2021@gmail.com>
2026-03-23 16:10:18 +0530

    Update AdminDashboard.jsx

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -457,7 +457,7 @@ export default function AdminControlPanel() {
         { id: "User Management", label: "User Management", icon: Users },
         { id: "Players", label: "Players Directory", icon: UserPlus },
         { id: "Applications", label: "Applications", icon: FileText },
-        { id: "Coach Management", label: "Coach Management", icon: UserCog },
+        { id: "Coach Management", label: "Secretary Management", icon: UserCog },
         { id: "Teams", label: "Teams", icon: Shield },
         { id: "Tournaments", label: "Tournaments", icon: Trophy },
         { id: "Referee Management", label: "Referee Management", icon: Flag },
